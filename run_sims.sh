25614523b985464d4fdbb3ee9326f0120f3c0875
Phoelionix <spencerkp@hotmail.com>
2023-05-02 21:24:05 +1000

    run sims update... carbon takes 30 mins now!

--- run_sims.sh
+++ run_sims.sh
@@ -1,8 +1,11 @@
 set -x
 #!/usr/bin/env
 
-./ac4dc input/dynamic_neutze/carbon_8keV
+./ac4dc input/dynamic_neutze/carbon_12keV
 ./ac4dc input/dynamic_neutze/carbon_10keV
-./ac4dc input/dynamic_neutze/lys_simple_6keV
+./ac4dc input/dynamic_neutze/carbon_8keV
+./ac4dc input/dynamic_neutze/carbon_6keV
 ./ac4dc input/dynamic_neutze/lys_simple_12keV
+./ac4dc input/dynamic_neutze/lys_simple_6keV
+
 
