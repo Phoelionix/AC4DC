b442c867012cfab7e5703145c96f3dd06d9dd63c
Phoelionix <spencerkp@hotmail.com>
2023-05-08 16:25:02 +1000

    plotting 2d R graphs

--- scripts/scattering/damage_comparison.py
+++ scripts/scattering/damage_comparison.py
@@ -12,7 +12,8 @@ import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
 import colorcet as cc; import cmasher as cmr
-from ..core_functions import get_sim_params
+from core_functions import get_sim_params
+import matplotlib.pyplot as plt
 
 PDB_PATHS = dict( # contains the target that should be used for the folder.
     neutze =  "/home/speno/AC4DC/scripts/scattering/2lzm.pdb",
@@ -39,7 +40,6 @@ def multi_damage(batch_handle,params,allowed_atoms_1,CNO_to_N,same_deviations,ch
     sim_input_dir = path.abspath(path.join(src_file_path ,"../../../input/")) + "/"
     # Folder containing the folders corresponding to each batch of handles.
     root_results_dir = path.join(src_file_path,"results_R_plotting/")+ "/"
-    print(root_results_dir,"ASDASDASDADSD")
     if path.isdir(root_results_dir):
         rmtree(root_results_dir)
     if path.isfile(root_results_dir):
@@ -65,38 +65,28 @@ def multi_damage(batch_handle,params,allowed_atoms_1,CNO_to_N,same_deviations,ch
     exp2_qualifier = "_ideal"        
 
     # find shape of array of params
-    energy_list = fwhm_list = photon_list = []
+    num_results = len(os.listdir(sim_data_batch_dir))
+    sim_handle_list, start_time, end_time, energy, fwhm, photon_count = ([None]*num_results for i in range(6))
     for i, sim_handle in enumerate(os.listdir(sim_data_batch_dir)):
-        start_time,end_time,e,f,p,param_dict,unit_dict = get_sim_params(sim_input_dir,sim_data_batch_dir,sim_handle)   
-        energy_list.append(e)
-        fwhm_list.append(f)
-        photon_list.append(p)
+        sim_handle_list[i] = sim_handle
+        start_time[i],end_time[i],energy[i],fwhm[i],photon_count[i],param_dict,unit_dict = get_sim_params(sim_input_dir,sim_data_batch_dir,sim_handle)
     
-    R_data_shape = ()
-    R_val_to_idx = []
-    for elem in energy_list,fwhm_list,photon_list:
-        arr = np.unique(np.array(elem))
-        R_data_shape += arr.shape
-        val_dict = {}
-        for i, val in enumerate(arr):
-            val_dict[val] = i
-        R_val_to_idx.append(val_dict)
     R_data = []
-    R_val_to_idx = {"energy":R_val_to_idx[0],"fwhm":R_val_to_idx[1],"photon_count":R_val_to_idx[2]}
-    
-    for i, sim_handle in enumerate(os.listdir(sim_data_batch_dir)):
+    for i, sim_handle in enumerate(sim_handle_list):
         run_params = copy.deepcopy(params)
-
+        
+        #TODO print properly
         print(sim_handle)
+        print(energy[i],fwhm[i],photon_count[i])
+        print(param_dict)
+        print(unit_dict)
         exp_name1 = sim_handle + "_" + exp1_qualifier
         exp_name2 = sim_handle + "_" + exp2_qualifier
 
-        start_time,end_time,energy,fwhm,photon_count = get_sim_params(sim_input_dir,sim_data_batch_dir,sim_handle)    
-
 
         # Spend a few billion on XFELs
-        experiment1 = XFEL(exp_name1,energy,**run_params["experiment"])
-        experiment2 = XFEL(exp_name2,energy,**run_params["experiment"])
+        experiment1 = XFEL(exp_name1,energy[i],**run_params["experiment"])
+        experiment2 = XFEL(exp_name2,energy[i],**run_params["experiment"])
         # Wait a few months for crystals to grow
 
         # For added realism
@@ -121,84 +111,76 @@ def multi_damage(batch_handle,params,allowed_atoms_1,CNO_to_N,same_deviations,ch
             crystal.plot_me(250000)
 
         if params["laser"]["SPI"]:
-            SPI_result1 = experiment1.spooky_laser(start_time,end_time,sim_handle,sim_data_batch_dir,crystal,results_parent_dir=results_batch_dir, **run_params["laser"])
-            SPI_result2 = experiment2.spooky_laser(start_time,end_time,sim_handle,sim_data_batch_dir,crystal_undmged,results_parent_dir=results_batch_dir, **run_params["laser"])
+            SPI_result1 = experiment1.spooky_laser(start_time[i],end_time[i],sim_handle,sim_data_batch_dir,crystal,results_parent_dir=results_batch_dir, **run_params["laser"])
+            SPI_result2 = experiment2.spooky_laser(start_time[i],end_time[i],sim_handle,sim_data_batch_dir,crystal_undmged,results_parent_dir=results_batch_dir, **run_params["laser"])
             R = stylin(exp_name1,exp_name2,experiment1.max_q,SPI=True,SPI_max_q = None,SPI_result1=SPI_result1,SPI_result2=SPI_result2)
         else:
-            exp1_orientations = experiment1.spooky_laser(start_time,end_time,sim_handle,sim_data_batch_dir,crystal, results_parent_dir=results_batch_dir, **run_params["laser"])
+            exp1_orientations = experiment1.spooky_laser(start_time[i],end_time[i],sim_handle,sim_data_batch_dir,crystal, results_parent_dir=results_batch_dir, **run_params["laser"])
             if exp_name2 != None:
                 # sync orientation with damage simulation
                 experiment2.set_orientation_set(exp1_orientations)  
                 run_params["laser"]["random_orientation"] = False 
-                experiment2.spooky_laser(start_time,end_time,sim_handle,sim_data_batch_dir,crystal_undmged, results_parent_dir=results_batch_dir, **run_params["laser"])
+                experiment2.spooky_laser(start_time[i],end_time[i],sim_handle,sim_data_batch_dir,crystal_undmged, results_parent_dir=results_batch_dir, **run_params["laser"])
             R = stylin(exp_name1,exp_name2,experiment1.max_q,SPI=False,results_parent_dir = results_batch_dir)
-        R_data.append([energy,fwhm,photon_count,R])
-        # R_data[ R_val_to_idx["energy"][energy],
-        #         R_val_to_idx["fwhm"][fwhm],
-        #         R_val_to_idx["photon_count"][photon_count]
-        #       ] = R        
+        R_data.append([energy[i],fwhm[i],photon_count[i],R]) 
     
     print("R_data:",R_data)
-    print(R_val_to_idx)
-    return np.array(R_data,dtype=np.float64)
-def plot_that_funky_thing(R_data):
-    # scatter_points = go.Scatter3d(
-    #     x=R_data[:,0], 
-    #     y=R_data[:,1], 
-    #     z=R_data[:,2], 
-    #     marker=go.scatter3d.Marker(size=6,color=R_data[:,3]), 
-    #     opacity=1, 
-    #     mode='markers',
-    # ) 
-    # fig=go.Figure(data=scatter_points)  
-    # fig.update_layout(
-    #     margin=dict(l=20,r=20,t=20,b=20),
-    #     scene = dict(
-    #         xaxis = {'title': {"text": 'energy'}},
-    #         yaxis = {'title': {"text": 'fwhm'}},
-    #         zaxis = {'title': {"text": 'photon count'}},
-    #     ),
-    #     legend=dict(
-    #         yanchor="top",
-    #         y=0.99,
-    #         xanchor="left",
-    #         x=0.01
-    #     ),        
-    # ) 
-    df = pd.DataFrame(dict(
-        energy =        R_data[:,0], 
-        fwhm =          R_data[:,1],
-        photon_count =  R_data[:,2],
-        R =             R_data[:,3],
-    ))
-    print(df)
-    # fig = px.scatter_3d(df, x='energy', y='fwhm', z='photon_count',
-    #           color='R', color_continuous_scale=[(0.1, "green"),(0, "green"), (0.2, "yellow"), (0.4, "red"),(1, "red")],range_color=[0,1])   
-    # fig = px.scatter_3d(df, x='energy', y='fwhm', z='photon_count',
-    #           color='R',  color_continuous_scale="PuRd", color_continuous_midpoint=0.2) #px.colors.diverging.Armyrose#"plasma"#"YlGnBu_r"#cc.m_fire#"inferno"#cmr.ghostlight#cmr.prinsenvlag_r#cmr.eclipse#cc.m_bjy#"viridis"#'Greys'#'binary'
-    fig = px.scatter_3d(df, x='energy', y='fwhm', z='photon_count',
-              color='R',  color_continuous_scale="amp", range_color=[0.1,0.3]) #px.colors.diverging.Armyrose#"plasma"#"YlGnBu_r"#cc.m_fire#"inferno"#cmr.ghostlight#cmr.prinsenvlag_r#cmr.eclipse#cc.m_bjy#"viridis"#'Greys'#'binary'
-    fig.show()    
-    
+    return np.array(R_data,dtype=np.float64)  
 
-# if __name__ == "__main__":
-#     allowed_atoms = ["C_fast","N_fast","O_fast","S_fast"]
-#     CNO_to_N = True
-#     same_deviations = True # whether same position deviations between damaged and undamaged crystal (SPI only)
-#     batch_dir = None # Optional: Specify existing parent folder for batch of results, to add these orientation results to.
-    
-#     R_data = multi_damage("tetra",imaging_params.default_dict,allowed_atoms,CNO_to_N,same_deviations,batch_dir)
-#     plot_that_funky_thing(R_data)
-#     print("Done")
-##%%
 allowed_atoms = ["C_fast","N_fast","O_fast","S_fast"]
 CNO_to_N = True
 same_deviations = True # whether same position deviations between damaged and undamaged crystal (SPI only)
 batch_dir = None # Optional: Specify existing parent folder for batch of results, to add these orientation results to.
 
 R_data = multi_damage("tetra",imaging_params.default_dict,allowed_atoms,CNO_to_N,same_deviations,batch_dir)
+
 ##%%
-plot_that_funky_thing(R_data)
+#%%
+def plot_that_funky_thing(R_data,cmin=0.1,cmax=0.3):
+    df = pd.DataFrame(dict(
+        photons =       R_data[:,2],
+        fwhm =          R_data[:,1],
+        energy =        R_data[:,0], 
+        R =             R_data[:,3],
+        dummy_column_for_size = R_data[:,0]*0+1
+    ))
+    print(df)
+    # fig = px.scatter_3d(df, x='energy', y='fwhm', z='photons',
+    #           color='R', color_continuous_scale=[(0.1, "green"),(0, "green"), (0.2, "yellow"), (0.4, "red"),(1, "red")],range_color=[0,1])   
+    # fig = px.scatter_3d(df, x='energy', y='fwhm', z='photons',
+    #           color='R',  color_continuous_scale="PuRd", color_continuous_midpoint=0.2) #px.colors.diverging.Armyrose#"plasma"#"YlGnBu_r"#cc.m_fire#"inferno"#cmr.ghostlight#cmr.prinsenvlag_r#cmr.eclipse#cc.m_bjy#"viridis"#'Greys'#'binary'
+    fig = px.scatter_3d(df, x='photons', y='fwhm', z='energy',
+              color='R',  color_continuous_scale="amp", range_color=[cmin,cmax]) #px.colors.diverging.Armyrose#"plasma"#"YlGnBu_r"#cc.m_fire#"inferno"#cmr.ghostlight#cmr.prinsenvlag_r#cmr.eclipse#cc.m_bjy#"viridis"#'Greys'#'binary'
+    fig.show()    
+    
+    unique_E = df.energy.unique()
+    #Store data frames
+    data_frame_dict = {elem : pd.DataFrame() for elem in unique_E}    
+    for key in data_frame_dict.keys():
+        data_frame_dict[key] = df[:][df.energy == key]    
+    for energy in unique_E:
+        df = data_frame_dict[energy] 
+        print("Energy:",energy,"eV")
+        fig = px.scatter(df,x='photons',y='fwhm',
+            color='R',  color_continuous_scale="amp", range_color=[cmin,cmax], size='dummy_column_for_size',size_max=15,)
+        fig.show()
+        # X,Y,Z = df['photons'].to_numpy(), df['fwhm'].to_numpy(),df['R'].to_numpy()
+        # Z_mesh = np.empty((len(X),len(Y)))
+        # for i,x in enumerate(X):
+        #         Z_mesh[i,i] = Z[i]
+
+        # plt.contourf([X, Y], Z,  alpha=0.7, cmap=plt.cm.jet)
+        # fig = go.Figure(data =
+        #     go.Contour(
+        #         z=[[10, 10.625, 12.5, 15.625, 20],
+        #         [5.625, 6.25, 8.125, 11.25, 15.625],
+        #         [2.5, 3.125, 5., 8.125, 12.5],
+        #         [0.625, 1.25, 3.125, 6.25, 10.625],
+        #         [0, 0.625, 2.5, 5.625, 10]],
+        #         colorscale='Electric',
+        #     ))
+        # fig.show()    
+plot_that_funky_thing(R_data,0,0.2)
 print("Done")
 
 # %%
--- scripts/scattering/imaging_params.py
+++ scripts/scattering/imaging_params.py
@@ -1,5 +1,8 @@
 from scatter import res_to_q
 
+print("====================")
+print("Refreshed imaging params")
+print("====================")
 
 top_resolution = 2
 bottom_resolution = None
@@ -17,7 +20,47 @@ default_dict = dict(
         positional_stdv = 0.2, # RMS in atomic coord position [angstrom] (set to 0 below if crystal, since rocking angle handles this aspect)
         include_symmetries = True,  # should unit cell contain symmetries?
         cell_packing = "SC",
-        rocking_angle = 0.5,  # (approximating mosaicity)
+        rocking_angle = 1.2,  # (approximating mosaicity)
+        orbitals_as_shells = True,
+        #CNO_to_N = True,   # whether the laser simulation approximated CNO as N  #TODO move this to indiv exp. args or make automatic
+    ),
+    #### XFEL params    
+    experiment = dict(
+        detector_distance_mm = 100,
+        screen_type = "flat",#"hemisphere"
+        q_minimum = res_to_q(bottom_resolution),#None #angstrom
+        q_cutoff = res_to_q(top_resolution),#2*np.pi/2
+        t_fineness=100,   
+        #####crystal stuff
+        max_triple_miller_idx = None, # = m, where max momentum given by q with miller indices (m,m,m)
+        ####SPI stuff
+        num_rings = 20,
+        pixels_per_ring = 20,
+        # first image orientation (cardan angles, degrees) 
+        SPI_x_rotation = 0,
+        SPI_y_rotation = 0,
+        SPI_z_rotation = 0,
+        #crystallographic orientations (not consistent with SPI yet)
+        # [ax_x,ax_y,ax_z] = vector parallel to rotation axis. Incompatible with random_orientations      
+        num_orients_crys=15,
+        orientation_axis_crys = None,#[1,1,0]        
+    ),
+)
+lys_dict = dict( 
+
+    #### Individual experiment arguments
+    laser = dict(
+        SPI = False,
+        pixels_across = 100,  # for SPI, shld go on xfel params.
+        random_orientation = True,  # NB: orientation is synced with undamaged crystal imaging  (TODO random orientation should not be separate from the other orient params...)
+    ),
+    ##### Crystal params
+    crystal = dict(
+        cell_scale = 3,  # for SC: cell_scale^3 unit cells 
+        positional_stdv = 0.2, # RMS in atomic coord position [angstrom] (set to 0 below if crystal, since rocking angle handles this aspect)
+        include_symmetries = True,  # should unit cell contain symmetries?
+        cell_packing = "SC",
+        rocking_angle = 0.15,  # (approximating mosaicity)
         orbitals_as_shells = True,
         #CNO_to_N = True,   # whether the laser simulation approximated CNO as N  #TODO move this to indiv exp. args or make automatic
     ),
