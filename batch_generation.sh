44a1e08248b05bef2c68fdefb9a7ab1275c7a7f0
Spencer Passmore <passmores@spartan-login3.hpc.unimelb.edu.au>
2024-03-21 11:49:01 +1100

    Adding files that for some reason weren't noted as changed

--- batch_generation.sh
+++ batch_generation.sh
@@ -2,4 +2,4 @@ set -x
 #!/usr/bin/env
 
 TAG=("N" "S" "Fe" "Se" "Kr" "I" "Gd")
-for t in ${TAG[@]}; do python3.9 generate_batch.py "input/superheavy/SH2_${t}/SH2_${t}.mol"; done
+for t in ${TAG[@]}; do python generate_batch.py "input/superheavy/SH2_${t}/SH2_${t}.mol"; done
