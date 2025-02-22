c1552a4ff5724bbd569aebc42cc8071e91230776
Phoelionix <spencerkp@hotmail.com>
2025-02-23 01:39:27 +1100

    Flesh out buccaneer pipeline

--- buccan.bash
+++ buccan.bash
@@ -11,6 +11,11 @@ ccp4_folder=/home/speno/CCP4_Workspace/lys_random_sample
 initial_model=$ccp4_folder/4et8_quite_incomplete.pdb
 sequence_model=$ccp4_folder/4et8.pdb
 
+num_init_bucc_cycles=3
+num_bucc_cycles=2
+num_refmacs_cycles=10
+num_pipeline_iters=15
+
 
 for tag in $(eval echo {1..$num_loops}); 
 do 
@@ -32,13 +37,70 @@ Y
 END
 eof-sftools
 
-	cd $ccp4_folder
-	mkdir -p model_solutions
-	mkdir -p buccaneer_logs
-	rm buccaneer_logs/${handle}_$tag.log # Note removes old log
+freerflag hklin $mtz_file hklout ${mtz_file}_tmp << eof-freerflag
+END
+eof-freerflag
 
-	buccaneer_pipeline -mtzin $mtz_file -pdbin $initial_model -seqin $sequence_model -colin-fo Fobs,Sigma-Fobs -pdbout model_solutions/${handle}_$tag.pdb >> buccaneer_logs/${handle}_$tag.log
+mv ${mtz_file}_tmp $mtz_file
 
+### Refinement pipeline, mimicing that of ccp4i2
+cd $ccp4_folder
+mkdir -p model_solutions
+mkdir -p buccaneer_logs
+rm buccaneer_logs/${handle}_$tag.log # Note removes old log
+
+refinement_model=model_solutions/${handle}_$tag.pdb
+refinement_model_tmp=model_solutions/${handle}_${tag}_tmp.pdb
+
+csheetbend -pdbin $initial_model -mtzin $mtz_file -pdbout $refinement_model -colin-fo Fobs,Sigma-Fobs -colin-free FreeR_flag -cycles 12 -resolution-by-cycle 6.0,3.0 -coord -radius-scale 4.0 
+
+for i in $(eval echo {1..$num_pipeline_iters}); do
+
+refmac5 xyzin $refinement_model xyzout $refinement_model_tmp hklin $mtz_file hklout refmac_out.mtz  << eof-refmac 
+NCYCLES 10
+WEIGHT AUTO
+MAKE HYDR NO
+REFI BREF ISOT
+MAKE NEWLIGAND NOEXIT
+SCALE TYPE SIMPLE
+SOLVENT YES
+PHOUT
+MONI DIST 1000000
+PDBOUT KEEP USERS
+LABIN FP=Fobs SIGFP=Sigma-Fobs FREE=FreeR_flag
+END
+eof-refmac
+
+mv $refinement_model_tmp $refinement_model
+
+#TODO do 3 initial bucc cycles
+
+#buccaneer_pipeline -mtzin $mtz_file -pdbin $refinement_model -pdbin-mr $refinement_model -seqin $sequence_model -pdbout $refinement_model_tmp  -colin-fo Fobs,Sigma-Fobs -colin-free FreeR_flag -buccaneer-anisotropy-correction -buccaneer-fast -buccaneer-1st-correlation-mode -buccaneer-nth-correlation-mode -buccaneer-resolution 1  -cycles $num_pipeline_iters -buccaneer-1st-cycles $num_init_bucc_cycles -buccaneer-nth-cycles $num_bucc_cycles -buccaneer-keyword "mr-model-filter-sigma 2.0" "model-filter-sigma 1.0" "find" "grow" >> buccaneer_logs/${handle}_$tag.log 
+buccaneer_pipeline -mtzin $mtz_file -pdbin $refinement_model -pdbin-mr $refinement_model -seqin $sequence_model -pdbout $refinement_model_tmp  -colin-fo Fobs,Sigma-Fobs -colin-free FreeR_flag -buccaneer-anisotropy-correction -buccaneer-fast -buccaneer-1st-correlation-mode -buccaneer-nth-correlation-mode -buccaneer-resolution 1  -cycles $num_bucc_cycles  -buccaneer-keyword "mr-model-filter-sigma 2.0" "model-filter-sigma 1.0" "find" "grow" >> buccaneer_logs/${handle}_$tag.log 
+
+# -buccaneer-keyword "mr-model-filter-sigma 2.0" "model-filter-sigma 1.0"
+
+mv $refinement_model_tmp $refinement_model
+
+
+refmac xyzin $refinement_model xyzout $refinement_model_tmp hklin $mtz_file hklout refmac_out.mtz  << eof-refmac 
+NCYCLES 10
+WEIGHT AUTO
+MAKE HYDR NO
+REFI BREF ISOT
+MAKE NEWLIGAND NOEXIT
+SCALE TYPE SIMPLE
+SOLVENT YES
+PHOUT
+MONI DIST 1000000
+PDBOUT KEEP USERS
+LABIN FP=Fobs SIGFP=Sigma-Fobs FREE=FreeR_flag
+END
+eof-refmac
+
+mv $refinement_model_tmp $refinement_model
+
+done; 
 done; 
 done;
 
