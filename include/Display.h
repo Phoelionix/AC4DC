4b9346b6c9364f83e4f397b5127abe55ba8b8219
Phoelionix <spencerkp@hotmail.com>
2025-06-15 02:33:07 +1000

    fix race condition by not doing reduction of Pdot_delta_subst

--- include/Display.h
+++ include/Display.h
@@ -29,7 +29,7 @@ This file is part of AC4DC.
 
 /**
  * @brief   // Curses implementation..
- * @details  This is purely to allow for ending the simualation with a key press. 
+ * @details  Allows for things like pausing the simulation and ending the simualation with a key press. 
  * @note Keep an eye on effect on computational time. Seems fine currently.
  * https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/keys.html
  */
