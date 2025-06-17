6d4b2b9630d7ca71e8e6b67f216e113151e692dc
Phoelionix <spencerkp@hotmail.com>
2025-06-17 20:58:58 +1000

    Turn off omp parllel seg fault suspect for now

--- src/ElectronRateSolver.cpp
+++ src/ElectronRateSolver.cpp
@@ -509,7 +509,8 @@ void ElectronRateSolver::sys_bound(const state_type& s, state_type& sdot, state_
         size_t N = Distribution::size; 
         auto t11 = std::chrono::high_resolution_clock::now();
 
-        #pragma omp parallel for num_threads(threads) reduction(+ : Pdot_delta_subst, Pdot_subst,sdot_bound_charge_eii_subst,sdot_bound_charge_tbr_subst)     
+        // Commenting out for now, might be causing rare segmentation faults. Uncomment if issue doesn't go away.
+        //#pragma omp parallel for num_threads(threads) reduction(+ : Pdot_delta_subst, Pdot_subst,sdot_bound_charge_eii_subst,sdot_bound_charge_tbr_subst)     
         for (size_t n=0; n<N; n++) {
             double tmp=0; // aggregator
             
